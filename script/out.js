e6d95f2e32e1788f0865e741b5b6524ece783f8f
Manjot singh <82448988+ManjotSingh4241@users.noreply.github.com>
2023-06-29 16:13:20 -0600

    Working Chatting Application

---
+++ script/out.js
@@ -0,0 +1,37 @@
+const socket = io('http://localhost:8080');
+
+const form = document.getElementById('msgbox');
+const messageInput = document.getElementById('msginp');
+const messageContainer = document.querySelector('#chatarea');
+
+const append = (message, position) => {
+  const messageElement = document.createElement('div');
+  messageElement.innerText = message;
+  messageElement.classList.add('msg');
+  messageElement.classList.add(position);
+  messageContainer.append(messageElement);
+};
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const message = messageInput.value;
+  append(`You: ${message}`, 'mr');
+  socket.emit('send', message);
+  messageInput.value = '';
+});
+
+const name = prompt('Enter your name to join');
+
+socket.emit('new-user-joined', name);
+
+socket.on('user-joined', (name) => {
+  append(`${name} joined the chat`, 'mr');
+});
+
+socket.on('receive', (data) => {
+  append(`${data.name}: ${data.message}`, 'ml');
+});
+
+socket.on('left', (data) => {
+    append(`${name} left the chat`, 'ml');
+  });
