56c3b9ab5692eee52472f3b63c832704d0aa8985
Manjot singh <82448988+ManjotSingh4241@users.noreply.github.com>
2023-06-29 16:14:14 -0600

    Working Server

--- Server/server.js
+++ Server/server.js
@@ -1,14 +1,35 @@
-const io = require('socket.io')(8080)
+var express = require("express");
+var app = express();
+var http = require("http").createServer(app);
+const io = require("socket.io")(http);
+var PORT = process.env.PORT || 8080;
+var path = require("path");
+
+app.use(express.static(path.join(__dirname, "../"))); 
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../index.html"));
+});
 
 const users = {};
 
-io.on('connection', socket => {
-    socket.on('new-user-joined', name =>{
-        users[socket.id] = name;
-        socket.broadcast.emit('user-joined', name);
-    })
+io.on("connection", (socket) => {
+  socket.on("new-user-joined", (name) => {
+    console.log("user:", name)
+    users[socket.id] = name;
+    socket.broadcast.emit("user-joined", name);
+  });
 
-    socket.on('send', message =>{
-        socket.broadcast.emit('receive', {message: message, name: users[socket.id]})
-    });
-})
+  socket.on("send", (message) => {
+    socket.broadcast.emit("receive", { message: message, name: users[socket.id] });
+  });
+
+  socket.on("disconnect", (message) => {
+    socket.broadcast.emit("left", users[socket.id]);
+    delete users[socket.id];
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`Express http server listening on port ${PORT}`);
+});
